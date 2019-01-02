33698c0e731ec9549512e602161dcd04c6908178
Aleksey Papushin <aleks.papushin@gmail.com>
2019-01-02 15:45:11 +0300

    added controller and action method

---
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : System.Web.Mvc.Controller
+    {
+        public string Detail()
+        {
+            return "Hello from a comic books controller";
+        }
+    }
+}
